83a5bdca39a72cbd0b1ca404f081aeaeb9cfe8bf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-01-18 23:23:37 -0500

    sample

--- customer-subscriptions/cypress/e2e/spec.cy.ts
+++ customer-subscriptions/cypress/e2e/spec.cy.ts
@@ -1,13 +1,12 @@
+// https://github.com/bahmutov/cypress-map
+import 'cypress-map'
+
 it('Activates a subscription', () => {
   cy.visit('/')
   cy.get('[data-cy=customer-item]', { timeout: 11_000 })
     .not('[data-status=active]')
-    .its('length')
-    .then((numberOfItems) => {
-      const randomItem = Cypress._.random(0, numberOfItems - 1)
-      cy.log(`Activating subscription for item #${randomItem}`)
-      cy.get('[data-cy=customer-item]').eq(randomItem).click()
-    })
+    .sample()
+    .click()
   cy.contains('Activate Subscription').click()
   cy.contains('Subscription was activated').should('be.visible')
 })
