943fc5e86534c743d6cbaf349c4820394c6135fa
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-01-19 10:19:57 -0500

    add if else

--- customer-subscriptions/cypress/e2e/spec.cy.ts
+++ customer-subscriptions/cypress/e2e/spec.cy.ts
@@ -1,5 +1,7 @@
 // https://github.com/bahmutov/cypress-map
 import 'cypress-map'
+// https://github.com/bahmutov/cypress-if
+import 'cypress-if'
 
 it('Activates a subscription', () => {
   cy.visit('/')
@@ -8,13 +10,20 @@ it('Activates a subscription', () => {
   // sample an item using cy.sample command
   // click on the item
   cy.get('[data-cy=customer-item]')
-    .not('[data-status=active]')
+    // the elements are there, the list will not change
+    // thus we can set the timeout to 0
+    // to quickly move to the next steps
+    .not('[data-status=active]', { timeout: 0 })
+    .if('exists')
     .sample()
     .click()
-  // find the button that contains the text "Activate Subscription"
-  // and click on it
-  cy.contains('Activate Subscription').click()
-  // the page should contain a visible element
-  // with text "Subscription was activated"
-  cy.contains('Subscription was activated').should('be.visible')
+    .then(() => {
+      // find the button that contains the text "Activate Subscription"
+      // and click on it
+      cy.contains('Activate Subscription').click()
+      // the page should contain a visible element
+      // with text "Subscription was activated"
+      cy.contains('Subscription was activated').should('be.visible')
+    })
+    .else('Could not find inactive subscription')
 })
