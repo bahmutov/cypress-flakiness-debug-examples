f18758c958f0a388e7ddfc935300efec5035f582
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-01-18 22:42:16 -0500

    initial test

--- customer-subscriptions/cypress/e2e/spec.cy.ts
+++ customer-subscriptions/cypress/e2e/spec.cy.ts
@@ -1,8 +1,10 @@
-it('Logs in the user', () => {
-  let randomItem = Math.floor(Math.random() * 7)
+it('Activates a subscription', () => {
+  const randomItem = Cypress._.random(1, 6)
 
   cy.visit('/')
-  cy.get('[data-cy=customer-item]').eq(randomItem).click()
+  cy.get('[data-cy=customer-item]', { timeout: 11_000 })
+    .eq(randomItem)
+    .click()
   cy.contains('Activate Subscription').click()
   cy.contains('Subscription was activated').should('be.visible')
 })
