d9d86006fb07b693473eaf2564b90b30bb370c43
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-01-18 22:03:22 -0500

    initial

---
+++ customer-subscriptions/cypress.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  projectId: 'hejdx4',
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    fixturesFolder: false,
+    supportFile: 'cypress/support.ts',
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
+    setupNodeEvents(on, config) {
+      return config
+    },
+  },
+})
